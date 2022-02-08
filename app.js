6e1f31a4e267c65b92e455b4e325ea0c1ca07707
AAoamk <60188000+AAoamk@users.noreply.github.com>
2022-02-08 14:14:15 +0200

    1.0.6

--- app.js
+++ app.js
@@ -3,19 +3,12 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3001
-const usersRouter = require('./controllers/user.controller')
+const usersRoute = require('./controllers/user.controller')
 console.log('connecting to', process.env.MONGODB_URI)
 mongoose.connect("mongodb+srv://websitecluster.t4wds.mongodb.net/websitecluster")
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
 
-app.listen(port,()=>console.log('hello world app listening'))
 app.use(cors())
 app.use(express.json())
-app.use('/api/users', usersRouter)
+app.use('/users.controller', usersRoute)
 
 module.exports = app
