3868512a1da02ed396159077a18d1c85639ed6cc
AAoamk <60188000+AAoamk@users.noreply.github.com>
2022-02-08 12:55:29 +0200

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express();
 const port = 3000;
-
+console.log('connecting to', process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 app.get('/', (req, res) => {
     res.send('hello world');
 })
