e223c757007034531536667711fdf84e60af7f0c
AAoamk <60188000+AAoamk@users.noreply.github.com>
2022-02-09 12:58:03 +0200

    1.1.0

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const http = require('http')
-
-const server = http.createServer(app)
-
-const port = process.env.port || 3000
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
