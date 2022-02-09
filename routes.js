ef5f31c21ac4baa324d82b7b0fb88c64d2715366
AAoamk <60188000+AAoamk@users.noreply.github.com>
2022-02-09 14:39:55 +0200

    1.1.5

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ router.get("/users", async (req, res) => {
 	const users = await Post.find()
 	res.send(users)
 })
+// add user
 router.post("/adduser", async (req, res) => {
 	const post = new Post({
 		username: req.body.username,
@@ -16,9 +17,35 @@ router.post("/adduser", async (req, res) => {
 	await post.save()
 	res.send(post)
 })
+// find user by username
 router.get("/users/:username", async (req, res) => {
+    try {
 	const post = await Post.findOne({ _id: req.params.id })
 	res.send(post)
+    } catch {
+    res.status(404)
+    res.send({ error: "Post doesn't exist!" })
+}
 })
 
+router.patch("/users/:username", async (req, res) => {
+	try {
+		const post = await Post.findOne({ _id: req.params.id })
+
+		if (req.body.email) {
+			post.title = req.body.title
+		}
+
+		if (req.body.password) {
+			post.content = req.body.content
+		}
+
+
+		await post.save()
+		res.send(post)
+	} catch {
+		res.status(404)
+		res.send({ error: "Post doesn't exist!" })
+	}
+})
 module.exports = router
