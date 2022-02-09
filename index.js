5dce517f151fa092b8c318993f1895cb80cde3af
AAoamk <60188000+AAoamk@users.noreply.github.com>
2022-02-09 19:00:26 +0200

    1.2.3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const http = require('http')
 const mongoose = require("mongoose")
 const routes = require("./routes")
-const PORT = process.env.PORT || 2000
+const PORT = process.env.PORT || 80
 
 // Connect to MongoDB database
 mongoose.connect("mongodb+srv://Snutri:tCbJtoBWdEHCOEjS@websitecluster.t4wds.mongodb.net/websitecluster?retryWrites=true&w=majority", { useNewUrlParser: true })
