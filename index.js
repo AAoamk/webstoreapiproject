2e73c3ede10867a395d1a535296cd1d3b07d5da3
AAoamk <60188000+AAoamk@users.noreply.github.com>
2022-02-23 13:45:11 +0200

    2.0.0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const server = http.createServer(app)
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('connected to MongoDB')
   })
